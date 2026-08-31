dd54fba79ba54cef3c83bf47191fe5aa9da710c2
Prince <princejaiswalpj251105@gmail.com>
2026-08-31 12:15:07 +0530

    Added typeinfo.cpp

---
+++ typeinfo.cpp
@@ -0,0 +1,82 @@
+// #include <bits/stdc++.h>
+// #include <typeinfo>
+// using namespace std;
+
+// int main() {
+//     auto i=0,*p=&i;
+// 	return 0;
+
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// #include <typeinfo>
+// using namespace std;
+
+// int main() {
+// 	int a=10,b=20;
+// 	auto sum=a+b;
+// 	cout<<sum<<endl; // 30
+// 	cout<<typeid(sum).name(); // i
+// 	return 0;
+
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// #include <typeinfo>
+// using namespace std;
+
+// int main() {
+//     int a[5]={2,3,4,5,6};
+//     for(int i=0;i<5;i++){
+//         cout<<a[i]<<endl;
+//     }
+// 	return 0;
+
+// } // normal array
+
+
+
+
+// #include <bits/stdc++.h>
+// #include <typeinfo>
+// using namespace std;
+
+// int main() {
+//     int array[5]={2,3,4,5,6};
+//     for(int a:array){
+//         cout<<a<<endl;
+//     }
+// 	return 0;
+
+// } // range based for loop 
+
+
+
+// #include <bits/stdc++.h>
+// #include <typeinfo>
+// using namespace std;
+
+// int main() {
+//     int array[5];
+//     for(auto &a:array){
+//         cout<<"enter some value";
+//         cin>>a;
+//     }for(int a:array){
+//         cout<<a<<endl;
+//     }
+// 	return 0;
+
+// }
+
+
+
+
+
+
+
