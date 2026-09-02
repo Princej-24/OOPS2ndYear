9cf3495ccb6900a7ec2f4288a991c927eb485d81
Prince <princejaiswalpj251105@gmail.com>
2026-09-02 14:31:56 +0530

    OOPS

---
+++ oopsday1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Structure for Complex Number
+struct Complex
+{
+    int real;
+    int imag;
+};
+
+int main()
+{
+    Complex c1;
+
+    cout << "Enter real part: ";
+    cin >> c1.real;
+
+    cout << "Enter imaginary part: ";
+    cin >> c1.imag;
+
+    cout << "Complex Number = " << c1.real << " + " << c1.imag << "i";
+
+    return 0;
+}
+cout << "Complex Number = "<</n<<Complex Number;
