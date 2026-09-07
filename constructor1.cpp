68d40d968e392c1e503979247af8388d537055b1
Prince <princejaiswalpj251105@gmail.com>
2026-09-07 12:00:23 +0530

    Constructor

---
+++ constructor1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Complex{
+    private:
+    int a, b;
+    public:
+    ~Complex(){
+        cout<<"Hello";
+    }
+};
+
+void fun(){
+    Complex obj;
+}
+
+int main() {
+    fun();
+}
