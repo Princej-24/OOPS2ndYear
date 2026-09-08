e2d012202ae551e6a205a8f38f6d0b589ca3ffe5
Prince <princejaiswalpj251105@gmail.com>
2026-09-08 11:49:06 +0530

    Home Coming

---
+++ new3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+template<typename X>
+X big(X a, X b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    cout << big(3.5, 4.2);
+    cout << endl;
+
+    cout << big(10, 20);
+    cout << endl;
+
+    cout << big(5.5f, 3.2f);
+
+    getch();
+    return 0;
+}
