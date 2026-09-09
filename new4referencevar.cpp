d3bd4b7bb8310353577a66bf33fb1df9632b7f17
Prince <princejaiswalpj251105@gmail.com>
2026-09-09 12:28:56 +0530

    Reference Variable

---
+++ new4referencevar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+
+    int &ref = a;
+
+    cout << a << endl;
+    cout << ref << endl;
+
+    ref = 20;
+
+    cout << a << endl;
+
+    return 0;
+}
