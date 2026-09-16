f68eae5bf19a4e5d07475219b00e790c8bbfc3ab
Prince <princejaiswalpj251105@gmail.com>
2026-09-16 14:22:40 +0530

    Function Overriding

--- Unit 3/funoverriding.cpp
+++ Unit 3/funoverriding.cpp
@@ -42,3 +42,30 @@ int main(){
     return 0;
 }
 // output = Prince
+
+// -------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class A{
+    void f1() {
+        cout<<"Prince";
+    };
+    void f2() {};
+};
+class B:public A{
+    public:
+    void f1(){
+         cout<<"Prince";
+    }; // function overriding
+    void f2(int x){
+        cout<<"Jaiswal"; // this will called by  f2
+    }; // function hiding
+};
+
+int main(){
+    B obj;
+    obj.f2(5); // if we call function 2 then we have to pass a parameter
+    return 0;
+}
