b1a7f2d7773a0af77523e300cc458d1515b9504f
Prince <princejaiswalpj251105@gmail.com>
2026-09-17 10:04:24 +0530

    Updated

--- Unit 3/ClassWork.cpp
+++ Unit 3/ClassWork.cpp
@@ -1,5 +1,7 @@
 // Create a base class shape and derived class circle, both will non-virtual function draw().Show that which function , compiler will call due to early binding in derived  class
 
+// by pointer
+
 #include <iostream>
 using namespace std;
 
@@ -24,6 +26,34 @@ int main() {
     ptr = &c;
 
     ptr->draw();
+
+    return 0;
+}
+
+// without pointer
+
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    void draw() {
+        cout << "Drawing Shape" << endl;
+    }
+};
+
+class Circle : public Shape {
+public:
+    void draw() {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+int main() {
+    Circle c;
+
+    c.draw();              // Circle's draw()
+    c.Shape::draw();       // Shape's draw()
 
     return 0;
 }
